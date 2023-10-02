3370dd6bee5b6ac13e54566533c5ec76cee1274b
Joel Flepp <joel_flepp@hotmail.com>
2023-10-02 16:56:42 +0200

    create static image application

--- src/app_static_image.py
+++ src/app_static_image.py
@@ -9,28 +9,38 @@
  * @Platform            Windows 11 Pro
  *
  * @Creation date:      25.09.2023
- * @Creator:            Joshua Stutz              
+ * @Creator:            Joshua Stutz & Joel Flepp
 """
 
 import cv2 as cv
 from common.processor import ImageProcessor
+from common.logger import Logger
+from common.image_reader import ImageReader
+
+
+class StaticImageApp:
+    def __init__(self, pathToImage):
+        self.pathToImage = pathToImage
+        self.imageProcessor = ImageProcessor()
+        self.logger = Logger(["Object", "Colora"])
+        self.imageReader = ImageReader()
+
+    def run(self):
+        try:
+            image = self.imageReader.readImage(self.pathToImage)
+            self.imageProcessor.loadImage(image)
+            self.imageProcessor.searchForPatterns()
+            self.logger.logDataFromPattern(self.imageProcessor.foundPatterns)
+            self.imageProcessor.displayProceedImg()
+            cv.waitKey()
+
+        except Exception as ex:
+            print(f"exception: {ex}")
+
+        finally:
+            cv.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    imgSrc = cv.imread("res/shapes-webcam-picture.jpg", cv.IMREAD_COLOR)
-
-    imageProcessor = ImageProcessor()
-    imageProcessor.loadImage(imgSrc)
-    imageProcessor.searchForPatterns()
-    imageProcessor.displayProceedImg()
-
-    """
-    try:
-        imageProcessor = ImageProcessor()
-        imageProcessor.loadImage(imgSrc)
-        imageProcessor.searchForPatterns()
-        imageProcessor.displayProceedImg()
-
-    except Exception as ex:
-        print(ex)
-    """
-    cv.waitKey(0)
+    app = StaticImageApp(pathToImage="res/shapes-webcam-picture.jpg")
+    app.run()
