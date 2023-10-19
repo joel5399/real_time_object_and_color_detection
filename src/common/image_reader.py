a20e579ef83eb7467e75fca679b29bfa8039fa07
Joshua Stutz <joshua.stutz2000@gmail.com>
2023-10-19 16:16:54 +0200

    add documentation for image_reader class

--- src/common/image_reader.py
+++ src/common/image_reader.py
@@ -1,11 +1,24 @@
 import cv2 as cv
 
+"""
+Class description:
+The ImageReader class loads an image from the filesystem.
+"""
+
 
 class ImageReader:
     def __init__(self):
+        """
+        Constructor
+        """
         pass
 
     def readImage(self, pathToImage):
+        """
+        Read an image from a given path and corresponding error handling.
+        :param pathToImage: path to the image as string
+        :return: image as cv.Mat()
+        """
         image = cv.imread(pathToImage, cv.IMREAD_COLOR)
         if len(image) == 0:
             raise Exception("could not load image!")
